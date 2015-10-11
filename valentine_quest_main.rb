abb18b49747b7751a562f9a45466b2325e3bdc1b
Martina Simicic <martina.simicic@gmail.com>
2015-10-11 21:43:28 +0200

    Draw background image

--- valentine_quest_main.rb
+++ valentine_quest_main.rb
@@ -4,12 +4,15 @@ class ValentineQuestMain < Gosu::Window
   def initialize
     super 1024, 768
     self.caption = "Valentine Quest"
+
+     @background_image = Gosu::Image.new("assets/images/gradient.png", :tileable => true)
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
 end
 
